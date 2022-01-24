717f44f428753785cbadc683065146f62f04e057
Ramya-6902 <97661977+Ramya-6902@users.noreply.github.com>
2022-01-24 09:14:48 +0530

    Add files via upload

---
+++ Exersice3.java
@@ -0,0 +1,14 @@
+package DatesAssignment;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+public class Exersice3 {
+	public static void main(String[]args) {
+		Date date =new Date();
+		SimpleDateFormat sdfmt = new SimpleDateFormat("E YYYY:MM:dd 'at' hh:mm:ss a zzz");
+		System.out.println("Current Date ="+sdfmt.format(date));
+		
+	}
+	
+
+}
